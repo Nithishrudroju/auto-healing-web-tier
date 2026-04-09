e1b18c475bb664292e02c135f1d3541c6ef0773f
Nithishrudroju <nithish00746@gmail.com>
2026-04-09 16:21:17 +0530

    docs: Update outputs to reference NLB DNS name instead of Elastic IP
    - Change elastic_ip output to nlb_dns_name
    - Update application_url to use NLB DNS for load-balanced access
    - Provides users with proper address for accessing the application across multiple instances

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
-output "elastic_ip" {
-  value       = aws_eip.web.public_ip
-  description = "Public IP address to access the application"
+output "nlb_dns_name" {
+  value       = aws_lb.web.dns_name
+  description = "DNS name of the Network Load Balancer"
 }
 
 output "application_url" {
-  value       = "http://${aws_eip.web.public_ip}"
+  value       = "http://${aws_lb.web.dns_name}"
   description = "Application URL"
 }
